f2c699a66dc608585b43e96d0790631621828fce
Shisato <shisatoyano@gmail.com>
2020-12-13 23:06:06 +0900

    implemented swap without temp value

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b) {
+    a = (a & b) + (a | b);
+
+    b = a + (~b) + 1;
+
+    a = a + (~b) + 1;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 5, b = 10;
+
+    cout << "Before swapping: a = " << a
+         << ", b = " << b << endl;
+
+    swap(a, b);
+
+    cout << "After swapping: a = " << a
+         << ", b = " << b << endl;
+
     return 0;
 }
